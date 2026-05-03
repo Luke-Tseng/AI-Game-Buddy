167b7ba95f855678694caa4f6f842e16746a24fe
Luke <lukegtseng@gmail.com>
2026-05-02 19:51:19 -0700

    Update cd files

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     trailingSlash: true,
-    eslint: {
-        ignoreDuringBuilds: false,
-    },
-    typescript: {
-        ignoreBuildErrors: false,
-    },
     images: {
         unoptimized: true,
     },
